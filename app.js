868181d6bf7bee3114e5c6481d5cd2cd394ecb85
Samuel-MESTIRI <samuelmestiri@hotmail.fr>
2020-06-02 16:46:05 +0200

    exercice8

--- app.js
+++ app.js
@@ -1,6 +1,10 @@
 var express = require('express'); 
 var http = require('http'); 
 var app = express(); 
+const bodyParser = require('body-parser')
+
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(bodyParser.json())
 
 function displayCurrentUrl(req) {
   console.log(req.protocol + '://' + req.get('host') + req.originalUrl)
@@ -16,8 +20,17 @@ app.get('/', function(req, res){
 app.get('/salut', function(req, res){   
   res.writeHead(200);   
   displayCurrentUrl(req)
-  res.write('salutt');   
+  res.write('salutt');    
   res.end(); 
 }); 
+
+app.get('/post', function (req, res) {
+  res.sendFile(__dirname + '/index.html')
+})
+
+app.post('/post', function (req, res) {
+  console.log('ca ne fonctionne pas', req.body)
+  res.send('le super post')
+})
 
 http.createServer(app).listen(4567);
