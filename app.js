f7b24bf47bfd9fd4d65337ce6382b1d90e809aba
Samuel-MESTIRI <samuelmestiri@hotmail.fr>
2020-06-02 18:07:18 +0200

    exercice4

--- app.js
+++ app.js
@@ -2,8 +2,9 @@ var http = require('http');
 http.createServer(function (request, response) {   
   response.writeHead(200);   
   // console.log(request)
-  console.log('http://localhost' + request.url)
-  response.write('<h1>L\'url est : '+ request.url + '</h1>\n');   
+  const lastIndex  = __dirname.split('\\').length
+  const name = __dirname.split('\\')[parseInt(lastIndex)-1] 
+  response.write('<h1>L\'url est : '+ request.url + '</h1>\n <p>Le chemin du repertoire : ' + __dirname + '</p>\n <p>Le nom du repertoire : ' + name + '</p>');   
   response.end(); 
 }).listen(5000); 
 console.log('Server running at ​http://localhost:5000​'); 
