6c98319616f57496dbe2ba81d187d1068aa1fa98
Samuel-MESTIRI <samuelmestiri@hotmail.fr>
2020-06-02 16:03:33 +0200

    exercice7

--- app.js
+++ app.js
@@ -2,14 +2,20 @@ var express = require('express');
 var http = require('http'); 
 var app = express(); 
 
+function displayCurrentUrl(req) {
+  console.log(req.protocol + '://' + req.get('host') + req.originalUrl)
+}
+
 app.get('/', function(req, res){   
   res.writeHead(200);   
+  displayCurrentUrl(req)
   res.write('Hello Express!!!');   
   res.end(); 
 }); 
 
 app.get('/salut', function(req, res){   
   res.writeHead(200);   
+  displayCurrentUrl(req)
   res.write('salutt');   
   res.end(); 
 }); 
